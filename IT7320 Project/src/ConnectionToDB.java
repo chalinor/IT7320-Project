779d18da3ec73faf2e615e8ff63235f13ebb9944
chalinor.baliuag <chatbaliuag@yahoo.com>
2018-07-25 11:13:24 +1200

    First commit-added Employee and ConnectionToDB classes.

---
+++ IT7320 Project/src/ConnectionToDB.java
@@ -0,0 +1,9 @@
+
+public class ConnectionToDB {
+
+	public static void main(String[] args) {
+		// code to connect to db
+
+	}
+
+}
---
+++ IT7320 Project/src/Employee.java
@@ -0,0 +1,42 @@
+
+public class Employee {
+	
+	private String employeeID;
+	private String firstName;
+	private String lastName;
+	private String position;
+	private String status;
+	
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
